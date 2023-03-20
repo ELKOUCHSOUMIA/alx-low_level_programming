5b4efae5cc71a63be5d78c0bf4b95aa7edb7cb91
ELKOUCHSOUMIA <somia1993elkouch@gmail.com>
2023-03-20 13:06:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
