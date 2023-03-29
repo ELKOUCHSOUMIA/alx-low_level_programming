0e04dc1ba8ce38f620f89388de5473c1c39016fe
ELKOUCHSOUMIA <somia1993elkouch@gmail.com>
2023-03-29 16:31:06 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * @n: int to chech
+ *
+ * Return: 0(success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
