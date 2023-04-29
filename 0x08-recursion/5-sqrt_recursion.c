43038164a4ef9f75f57ee8c432e812bbb4634914
ELKOUCHSOUMIA <somia1993elkouch@gmail.com>
2023-04-29 15:13:03 -0700

    0-create_array.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square of
+ *
+ * Return: result of the square
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return(
